2f22cfa6cc09edbc87b33e0360ea26f4974bc4d6
naritomo <naritomo08@gmail.com>
2023-06-09 23:00:23 +0900

    SSM対応化

--- aws/resource.tf
+++ aws/resource.tf
@@ -126,6 +126,89 @@ resource "aws_eip" "example" {
   vpc      = true
 }
 
+# ポリシー作成(SSM許可)
+
+resource "aws_iam_role_policy" "instance_role_policy" {
+    name = "EC2SSMPolicy_apply"
+    role = "${aws_iam_role.instance_role.id}"
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:DescribeAssociation",
+                "ssm:GetDeployablePatchSnapshotForInstance",
+                "ssm:GetDocument",
+                "ssm:DescribeDocument",
+                "ssm:GetManifest",
+                "ssm:GetParameter",
+                "ssm:GetParameters",
+                "ssm:ListAssociations",
+                "ssm:ListInstanceAssociations",
+                "ssm:PutInventory",
+                "ssm:PutComplianceItems",
+                "ssm:PutConfigurePackageResult",
+                "ssm:UpdateAssociationStatus",
+                "ssm:UpdateInstanceAssociationStatus",
+                "ssm:UpdateInstanceInformation"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssmmessages:CreateControlChannel",
+                "ssmmessages:CreateDataChannel",
+                "ssmmessages:OpenControlChannel",
+                "ssmmessages:OpenDataChannel"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2messages:AcknowledgeMessage",
+                "ec2messages:DeleteMessage",
+                "ec2messages:FailMessage",
+                "ec2messages:GetEndpoint",
+                "ec2messages:GetMessages",
+                "ec2messages:SendReply"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+# ロール作成(SSM許可)
+
+resource "aws_iam_role" "instance_role" {
+    name = "EC2SSMRole_apply"
+    assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+EOF
+}
+
+# ロール指定プロファイル作成(SSM許可)
+resource "aws_iam_instance_profile" "instance_role" {
+    name = "SSM_apply_profile"
+    role = aws_iam_role.instance_role.name
+}
+
 # ---------------------------
 # EC2
 # ---------------------------
@@ -144,6 +227,7 @@ resource "aws_instance" "aws_ec2" {
   subnet_id                   = aws_subnet.public_1a.id
   associate_public_ip_address = "false"
   key_name                    = "serverkey"
+  iam_instance_profile = "SSM_apply_profile"
   tags = {
     Name = "vm01"
   }
