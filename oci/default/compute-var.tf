488b69861bd67849adcc75a0483875a9b17d1e5b
naritomo <86882121+naritomo08@users.noreply.github.com>
2026-09-10 22:08:29 +0900

    Delete ssh_public_key_path variable
    Removed ssh_public_key_path variable definition.

--- oci/default/compute-var.tf
+++ oci/default/compute-var.tf
@@ -13,6 +13,3 @@ variable "instance_image_ocid" {
     ap-osaka-1 = "ocid1.image.oc1.ap-osaka-1.aaaaaaaa7slxd3wjvwuer2crlmm5qz3g2y2yma2ru3hhffyqi4bvk6mzu2aa"
   }
 }
-variable "ssh_public_key_path" {
-  default = "../apikey/id_server_rsa.pub"
-}
