371ae04ab742a8e3f5e63556979c8fedf3f993ce
naritomo <86882121+naritomo08@users.noreply.github.com>
2026-09-10 21:40:47 +0900

    Update backend configuration for Terraform
    Commented out S3 backend configuration and added OCI backend configuration.

--- oci/default/backend.tf
+++ oci/default/backend.tf
@@ -1,14 +1,8 @@
-# ftstateをバケットに保管する。
 terraform {
-  #backend "s3" {
-  #  bucket   = "tfstate_bucket"
-  #  key      = "terraform.tfstate"
-  #  profile  = "oci_access"
-  #  region   = "<使用しているリージョンの識別子>"
-  #  endpoint = "https://<テナンシのオブジェクト・ストレージ・ネームスペース>.compat.objectstorage.<使用しているリージョンの識別子>.oraclecloud.com"    
-  #  skip_region_validation      = true
-  #  skip_credentials_validation = true
-  #  skip_metadata_api_check     = true
-  #  force_path_style            = true
+  #backend "oci" {
+    #bucket    = "tfstate_bucket"
+    #namespace = "${var.tenancy_namespace}"
+    #key       = "terraform.tfstate"
+    #auth      = "APIKey"
   #}
 }
